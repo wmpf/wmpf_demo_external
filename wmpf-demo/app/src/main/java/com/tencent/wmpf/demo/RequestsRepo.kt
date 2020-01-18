fd0a46e36713ca71099deca9fe3c91ee685cea5d
complexzeng <complexzeng@tencent.com>
2020-01-18 21:26:35 +0800

    feat(demo):new active flow

--- wmpf-demo/app/src/main/java/com/tencent/wmpf/demo/RequestsRepo.kt
+++ wmpf-demo/app/src/main/java/com/tencent/wmpf/demo/RequestsRepo.kt
@@ -1,6 +1,7 @@
 package com.tencent.wmpf.demo
 
 import android.util.Log
+import com.tencent.mmkv.MMKV
 import com.tencent.wmpf.cli.task.IPCInovkerTask_SetPushMsgCallback
 import com.tencent.wmpf.cli.task.IPCInvokerTask_getPushToken
 import com.tencent.wmpf.cli.task.pb.WMPFBaseRequestHelper
@@ -10,22 +11,19 @@ import com.tencent.wmpf.proto.WMPFPushMsgRequest
 import com.tencent.wmpf.proto.WMPFPushMsgResponse
 import com.tencent.wmpf.proto.WMPFPushTokenRequest
 import com.tencent.wmpf.proto.WMPFPushTokenResponse
+import okhttp3.FormBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.FormBody
 import org.json.JSONObject
 import java.io.IOException
-import java.lang.Exception
 import java.nio.charset.Charset
 
 
 /**
  * Created by complexzeng on 2019-10-17 16:25.
  */
-class RequestsRepo {
-    companion object {
-        const val TAG = "RequestsRepo"
-    }
+object RequestsRepo {
+    const val TAG = "RequestsRepo"
 
     private val client = OkHttpClient()
 
@@ -36,8 +34,8 @@ class RequestsRepo {
                 val response = client.newCall(request).execute()
                 if (response.isSuccessful) {
                     try {
-                        val resp = response.body()!!.source().readString(Charset.defaultCharset())
-                        val jsonObject= JSONObject(resp)
+                        val respBody = response.body()!!.source().readString(Charset.defaultCharset())
+                        val jsonObject = JSONObject(respBody)
                         val accessToken = jsonObject.optString("access_token")
                         callback(true, accessToken)
                     } catch (e: Exception) {
@@ -49,6 +47,58 @@ class RequestsRepo {
                 }
             } catch (e: Exception) {
                 Log.e(TAG, "getAccessToken: ", e)
+            }
+        }.start()
+    }
+
+    const val KEY_TEST_PRODUCT_ID = "product_id"
+
+    const val KEY_TEST_DEVICE_ID = "device_id"
+
+    const val KEY_TEST_SIGNATURE = "signature"
+
+    const val KEY_TEST_KEY_VERSION = "key_version"
+
+    const val KEY_EXPIRED_TIME_MS = "expiredTimeMs"
+
+    /**
+     * @param ticket  get from wecooper
+     * @param wxaAppId get from wecooper
+     * only for test
+     * never use this function for production environment
+     */
+    fun getTestDeviceInfo(ticket: String, wxaAppId: String, hostAppId: String, callback: (resp: String) -> Unit) {
+        Thread {
+            val req = Request.Builder().url("https://open.weixin.qq.com/wxaruntime" +
+                    "/getdemodeviceinfo?ticket=$ticket&wxaappid=$wxaAppId&hostappid=$hostAppId").build()
+            try {
+                val resp = client.newCall(req).execute()
+                if (resp.isSuccessful) {
+                    val respBody = resp.body()!!.source().readString(Charset.defaultCharset())
+                    callback(respBody)
+                    val jsonObject = JSONObject(respBody)
+                    val productId = jsonObject.optInt(KEY_TEST_PRODUCT_ID, 0)
+                    val deviceId = jsonObject.optString(KEY_TEST_DEVICE_ID, "")
+                    val signature = jsonObject.optString(KEY_TEST_SIGNATURE, "")
+                    val keyVersion = jsonObject.optInt(KEY_TEST_KEY_VERSION, 0)
+                    val appIdList = jsonObject.optJSONArray("appid_list")
+                    val expiredTimeMs = jsonObject.optLong(KEY_EXPIRED_TIME_MS) * 1000L + System.currentTimeMillis()
+                    Log.d(TAG, "getDeviceInfo: productId = $productId, deviceId = $deviceId, signature = $signature, keyVersion = $keyVersion, appIdList = $appIdList, expiredTimeMs = $expiredTimeMs")
+
+                    val kv = MMKV.mmkvWithID(TAG, MMKV.SINGLE_PROCESS_MODE)
+
+                    kv.putInt(KEY_TEST_PRODUCT_ID, productId)
+                    kv.putString(KEY_TEST_DEVICE_ID, deviceId)
+                    kv.putString(KEY_TEST_SIGNATURE, signature)
+                    kv.putInt(KEY_TEST_KEY_VERSION, keyVersion)
+                    kv.putLong(KEY_EXPIRED_TIME_MS, expiredTimeMs)
+                } else {
+                    callback("error")
+                    Log.w(TAG, "getDeviceInfo fail: ")
+                }
+            } catch (e: Exception) {
+                callback("error: ${e.message}")
+                Log.e(TAG, "getDeviceInfo fail: ${e.message}")
             }
         }.start()
     }
