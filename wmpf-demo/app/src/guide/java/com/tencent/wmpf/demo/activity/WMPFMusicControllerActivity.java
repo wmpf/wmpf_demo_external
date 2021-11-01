4c6827eb0b296245e183b3aaf7ca71c54ff76609
complexzeng <complexzeng@tencent.com>
2021-11-01 15:59:15 +0800

    feat(wmpf-demo): query is playing

--- wmpf-demo/app/src/guide/java/com/tencent/wmpf/demo/activity/WMPFMusicControllerActivity.java
+++ wmpf-demo/app/src/guide/java/com/tencent/wmpf/demo/activity/WMPFMusicControllerActivity.java
@@ -4,15 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-import com.tencent.wmpf.cli.api.WMPCliDefaultExecutor;
+import com.tencent.wmpf.cli.api.WMPFClientDefaultExecutor;
 import com.tencent.wmpf.cli.api.WMPFMusicController;
 import com.tencent.wmpf.demo.Api;
 import com.tencent.wmpf.demo.R;
 
 public class WMPFMusicControllerActivity extends AppCompatActivity {
 
-    private final WMPFMusicController musicController = new WMPFMusicController(new WMPCliDefaultExecutor());
+    private final WMPFClientDefaultExecutor executor = new WMPFClientDefaultExecutor();
+    private final WMPFMusicController musicController = new WMPFMusicController(executor);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class WMPFMusicControllerActivity extends AppCompatActivity {
         Button previousBtn = findViewById(R.id.btn_previous);
         Button nextBtn = findViewById(R.id.btn_next);
         Button startQQMusicBtn = findViewById(R.id.btn_start_qq_music);
+        final Button queryIsPlaying = findViewById(R.id.is_playing);
 
         playOrPauseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +52,28 @@ public class WMPFMusicControllerActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Api.INSTANCE.launchWxaApp("wxada7aab80ba27074", "", 0, 0).subscribe();
+            }
+        });
+
+        queryIsPlaying.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        final boolean isPlaying = musicController.isPlaying();
+                        queryIsPlaying.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(WMPFMusicControllerActivity.this,
+                                        String.format("isPlaying = [%b]", isPlaying),
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+                });
+                thread.start();
             }
         });
 
