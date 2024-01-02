cba879cb250c89428c34858c1cf0a5fb7f3c9fd5
jaxonzhao <jaxonzhao@tencent.com>
2024-01-02 16:00:33 +0800

    feat: landscape demo

--- wmpf-demo/app/src/main/java/com/tencent/wmpf/demo/ui/FastExperienceActivity.kt
+++ wmpf-demo/app/src/main/java/com/tencent/wmpf/demo/ui/FastExperienceActivity.kt
@@ -3,13 +3,18 @@ package com.tencent.wmpf.demo.ui
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.RadioGroup
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.tencent.wmpf.cli.api.WMPF
 import com.tencent.wmpf.cli.api.WMPFAccountApi
 import com.tencent.wmpf.cli.api.WMPFApiException
+import com.tencent.wmpf.cli.api.WMPFMiniProgramApi.LandscapeMode
 import com.tencent.wmpf.cli.model.WMPFStartAppParams
 import com.tencent.wmpf.cli.model.WMPFStartAppParams.WMPFAppType
 import com.tencent.wmpf.demo.R
@@ -52,11 +57,30 @@ class FastExperienceActivity : AppCompatActivity() {
             }
         }
 
+        var landscapeMode = LandscapeMode.NORMAL
+        findViewById<Spinner>(R.id.choose_landscape).apply {
+            this.adapter = ArrayAdapter.createFromResource(
+                this@FastExperienceActivity,
+                R.array.landscapes,
+                R.layout.support_simple_spinner_dropdown_item
+            )
+            this.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(p0: AdapterView<*>?, view: View?, p2: Int, p3: Long) {
+                    landscapeMode = LandscapeMode.values()[p2]
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    landscapeMode = LandscapeMode.NORMAL
+                }
+
+            }
+        }
+
         findViewById<Button>(R.id.btn_launch_wxa_app).setOnClickListener {
             val appId = appIdView.text.toString()
             perf.edit().putString("appId", appId).apply()
             execute {
-                launchMiniProgram(appId, appType)
+                launchMiniProgram(appId, appType, landscapeMode)
             }
         }
 
@@ -96,7 +120,11 @@ class FastExperienceActivity : AppCompatActivity() {
         }
     }
 
-    private fun launchMiniProgram(appId: String, type: WMPFAppType = WMPFAppType.APP_TYPE_RELEASE) {
+    private fun launchMiniProgram(
+        appId: String,
+        type: WMPFAppType = WMPFAppType.APP_TYPE_RELEASE,
+        landscapeMode: LandscapeMode = LandscapeMode.NORMAL
+    ) {
         logger.clear()
         if (type == WMPFAppType.APP_TYPE_EXP || type == WMPFAppType.APP_TYPE_DEV) {
             if (!WMPF.getInstance().accountApi.isLogin) {
@@ -107,7 +135,7 @@ class FastExperienceActivity : AppCompatActivity() {
         logger.i("启动小程序：$appId")
         try {
             WMPF.getInstance().miniProgramApi.launchMiniProgram(
-                WMPFStartAppParams(appId, "", type)
+                WMPFStartAppParams(appId, "", type), false, landscapeMode
             )
         } catch (e: WMPFApiException) {
             logger.e("启动小程序失败", e)
